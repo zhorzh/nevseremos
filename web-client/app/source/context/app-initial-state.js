90400a5be6f016a2f419d8ae915792f422273663
Alexandr Zhorzh <zhorzh.alexandr@gmail.com>
2020-05-25 23:05:15 +0300

    feature(web-client): create app initial state

---
+++ web-client/app/source/context/app-initial-state.js
@@ -0,0 +1,7 @@
+export let initial_state = {
+  API_Status: 'not checked',
+  tab: '/',
+  'waitingForResponse': false,
+  snackbar: {IsOpen: false, message: '', variant: 'info'},
+  drawer: {isOpen: false},
+}
